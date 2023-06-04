790c08864e6aaa3873cf4d9bdf33bc8931fa4f45
EEstaniel <edwineestaniel93@gmail.com>
2023-06-04 02:55:28 -0700

    feat: create language abbrev. to full name

---
+++ src/util/ConvertLanguage.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const ConvertLanguage = (langauge: string) => {
+    const commonMovieLanguages: {[key: string]: string} = {
+        en: "English",
+        es: "Spanish",
+        zh: "Chinese",
+        hi: "Hindi",
+        ja: "Japanese",
+        fr: "French",
+        de: "German",
+        ru: "Russian",
+        ko: "Korean",
+        pt: "Portuguese",
+        it: "Italian",
+        ar: "Arabic",
+        tr: "Turkish",
+        pl: "Polish",
+        nl: "Dutch",
+        sv: "Swedish",
+        fa: "Persian",
+        da: "Danish",
+        no: "Norwegian",
+        fi: "Finnish",
+        he: "Hebrew",
+        el: "Greek",
+        cs: "Czech",
+        th: "Thai",
+        id: "Indonesian",
+        vi: "Vietnamese",
+        hu: "Hungarian",
+    };
+
+    return commonMovieLanguages[langauge];
+}
+export default ConvertLanguage;
