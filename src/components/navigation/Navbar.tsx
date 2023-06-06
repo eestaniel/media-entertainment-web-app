f5e60ed17f3b4e47e3685e2a027b4419bb0cd680
EEstaniel <edwineestaniel93@gmail.com>
2023-06-05 17:10:11 -0700

    fix: css / icons for navigation mobile view

--- src/components/navigation/Navbar.tsx
+++ src/components/navigation/Navbar.tsx
@@ -1,31 +1,38 @@
 import React from 'react';
 import {Link, useLocation} from 'react-router-dom';
+import './styles/Navbar.scss'
 
 const Navbar = () => {
     return (
         <React.Fragment>
             {/* Navbar: home, movie, tv */}
             <nav className="navbar">
-                <div className="navbar__logo">
-                    <h1>Media<span>Hub</span></h1>
-                </div>
+                <div className="navbar__logo" datatype={'logo'}></div>
                 <div className="navbar__links">
                     <ul>
                         <li>
-                            <Link to="/" className="navbar__link">Home</Link>
+                            <Link to="/" className="navbar__link">
+                                <div className="navbar__link__icon" datatype={'home'}></div>
+                            </Link>
                         </li>
                         <li>
-                            <Link to="/movie" className="navbar__link">Movies</Link>
+                            <Link to="/movie" className="navbar__link">
+                                <div className="navbar__link__icon" datatype={'movies'}></div>
+
+                            </Link>
                         </li>
                         <li>
-                            <Link to="/tv" className="navbar__link">TV Shows</Link>
+                            <Link to="/tv" className="navbar__link">
+                                <div className="navbar__link__icon" datatype={'tv'}></div>
+                            </Link>
                         </li>
 
                     </ul>
                 </div>
+                <div className="navbar__avatar" datatype={'avatar'}></div>
             </nav>
         </React.Fragment>
-);
+    );
 };
 
 export default Navbar;
